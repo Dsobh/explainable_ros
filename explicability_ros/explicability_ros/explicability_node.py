a70433d5b4967f6bda6e9f3eca48a50082494ee4
dsobh <davidsobrin@gmail.com>
2024-03-05 18:19:52 +0100

    Revert "Mods to read cmd_vel msgs"
    This reverts commit d693803627da1135647b0efaeba598beec3f0d08.

--- explicability_ros/explicability_ros/explicability_node.py
+++ explicability_ros/explicability_ros/explicability_node.py
@@ -6,8 +6,7 @@ import rclpy
 from simple_node import Node
 from rclpy.callback_groups import ReentrantCallbackGroup
 
-# from rcl_interfaces.msg import Log
-from geometry_msgs.msg import Twist
+from rcl_interfaces.msg import Log
 from builtin_interfaces.msg import Time
 from explicability_msgs.srv import Question
 from explicability_ros.task_creation_chain import TaskCreationChain
@@ -26,7 +25,7 @@ class ExplainabilityNode(Node):
         self.total_time = 0
         self.embedding_number = 0
         self.msg_queue = []
-        # self.previous_msg = ""
+        self.previous_msg = ""
 
         # Para gestionar una cola de los 10 mensajes mas recientes y evitar demasiados duplicados
         self.recent_msgs = []
@@ -49,21 +48,18 @@ class ExplainabilityNode(Node):
             callback_group=ReentrantCallbackGroup())
 
         self.subscription = self.create_subscription(
-            Twist,
-            "/input_joy/cmd_vel",
+            Log,
+            "/rosout",
             self.listener_callback,
             1000,
             callback_group=ReentrantCallbackGroup())
 
         self.emb_timer = self.create_timer(0.001, self.emb_cb)
 
-    def twist2text(self, twist: Twist):
-        return f" linear(x:{twist.linear.x},y:{twist.linear.y},z: {twist.linear.z}) - angular(x:{twist.angular.x},y: {twist.angular.y},z: {twist.angular.z})"
-
-    def listener_callback(self, log: Twist) -> None:
+    def listener_callback(self, log: Log) -> None:
         self.logs_number += 1
         self.msg_queue.append(log)
-        print(f"Log {self.logs_number}: {self.twist2text(log)}")
+        print(f"Log {self.logs_number}: {log.msg}")
 
     def emb_cb(self) -> None:
 
@@ -72,23 +68,20 @@ class ExplainabilityNode(Node):
             start = time.time()
 
             # Eliminar solo el mensaje anterior
-            # if log.msg != self.previous_msg:
+            if log.msg != self.previous_msg:
+                msg_sec = log.stamp.sec
+                msg_nanosec = log.stamp.nanosec
+                unix_timestamp = msg_sec + msg_nanosec / 1e9
 
-            # El tipo twist no tiene atributo de timestamp
-            # msg_sec = log.stamp.sec
-            # msg_nanosec = log.stamp.nanosec
-            # unix_timestamp = msg_sec + msg_nanosec / 1e9
+                self.db.add_texts(
+                    texts=[str(unix_timestamp) + " - " + log.msg])
+                self.previous_msg = log.msg
+                self.embedding_number += 1
 
-            self.db.add_texts(
-                # texts=[str(unix_timestamp) + " - " + self.twist2text(log)])
-                texts=[self.twist2text(log)])
-            # self.previous_msg = log.msg
-            self.embedding_number += 1
-
-            emb_time = time.time() - start
-            self.total_time += emb_time
-            print(
-                f"Time to create embedding {self.embedding_number}: {emb_time} | Total time: {self.total_time}")
+                emb_time = time.time() - start
+                self.total_time += emb_time
+                print(
+                    f"Time to create embedding {self.embedding_number}: {emb_time} | Total time: {self.total_time}")
 
     def order_retrievals(self, docuemnt_list):
 
@@ -113,6 +106,9 @@ class ExplainabilityNode(Node):
 
         for i in sortered_list:
             logs += i
+
+        # for d in docuemnts:
+        #    logs += d.page_content + "\n"
 
         answer = self.question_chain.run(logs=logs, question=request.question)
 
